88a5692805ac72c2cc90e934090e3624d4d4c96d
Abhishek kumar <47034350+qazwsxedcrfv12@users.noreply.github.com>
2022-03-08 17:24:58 +0530

    Height
    Height of the tree. ( Defined as: The total no of nodes in longest path from root to leaf node)

--- Tree/Height-of-tree/height.java
+++ Tree/Height-of-tree/height.java
@@ -1,1 +1,48 @@
 
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class heightOfTree {
+    public static Node insert(Node root, int data){
+        Node temp = new Node(data);
+        if(root==null) {
+            root = temp;
+            return root;
+        }else if(root.data <= data) root.right = insert(root.right, data); else root.left = insert(root.left, data);
+        return root;
+    }
+
+    public static void print(Node root){
+        if(root==null) return;
+        print(root.left);
+        System.out.print(root.data + " ");
+        print(root.right);
+    }
+
+    public static int calculateHeight(Node root){
+        if(root==null) return 0;
+        return Math.max(calculateHeight(root.left), calculateHeight(root.right)) + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Node root = null;
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            root = insert(root, sc.nextInt());
+        }
+        int height = calculateHeight(root);
+        System.out.println("The height of the given tree is: "+ height);
+    }
+}
